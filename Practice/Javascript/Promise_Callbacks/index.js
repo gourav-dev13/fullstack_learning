f5a3b5eead5eefc2ede4e7e080d2b7c69fe76756
gourav_dev13 <gouravpal5513@gmail.com>
2026-04-24 11:25:12 +0530

    chore : practice API fetch usingpublic api key

--- Practice/Javascript/Promise_Callbacks/index.js
+++ Practice/Javascript/Promise_Callbacks/index.js
@@ -8,54 +8,54 @@ function callback(a, b, c){
 callback( 2 , 5 , sum)
 
 // callback Hell
-// function getData( dataId , getnextData){
-//     setTimeout(()=>{
-//         console.log("data : ", dataId);
-//         if(getnextData){
-//             getnextData();
-//         }
-//     },2000);
-// }
+function getData( dataId , getnextData){
+    setTimeout(()=>{
+        console.log("data : ", dataId);
+        if(getnextData){
+            getnextData();
+        }
+    },2000);
+}
  
-// getData( 1 , ()=>{
-//     getData(2 , ()=>{
-//         getData(3 ,()=>{
-//             getData(4);
-//         })
-//     })
-// });
+getData( 1 , ()=>{
+    getData(2 , ()=>{
+        getData(3 ,()=>{
+            getData(4);
+        })
+    })
+});
 
 
 // Promises
-// let promise = new Promise((resolve,reject)=>{
-//     console.log("I am a  promise");
-//     try{
-//          resolve("this is resolved promise");
-//         }
-//     catch{
-//             reject("error occurerd");
-//         }
-// });
+let promise = new Promise((resolve,reject)=>{
+    console.log("I am a  promise");
+    try{
+         resolve("this is resolved promise");
+        }
+    catch{
+            reject("error occurerd");
+        }
+});
 
 // Handling promises with .then
-// function getdata(dataId){
-//     return new Promise((resolve , reject)=>{
-//         setTimeout(()=>{
-//             console.log("api's Data : " , dataId);
-//             resolve("success");
-//         },2000);
-//     });
-// }
+function getdata(dataId){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            console.log("api's Data : " , dataId);
+            resolve("success");
+        },2000);
+    });
+}
 
-// getdata(1).then(() =>{
-//     console.log("complete 1st promise");
-//     getdata(2).then(()=>{
-//         console.log("complete 2nd promise");
-//         getdata(3).then(()=>{
-//             console.log("complete 3rd promise");
-//         })
-//     })
-// }); 
+getdata(1).then(() =>{
+    console.log("complete 1st promise");
+    getdata(2).then(()=>{
+        console.log("complete 2nd promise");
+        getdata(3).then(()=>{
+            console.log("complete 3rd promise");
+        })
+    })
+}); 
 
 // async Await
 function getData(id) {
@@ -80,3 +80,16 @@ async function fetchData() {
 }
 
 fetchData();
+
+// eg with real api 
+async function GETDATA(){
+    console.log("getting Data");
+    let data = await fetch('https://jsonplaceholder.typicode.com/todos');
+    let realdata = await data.json();
+    for(let i = 0; i< 10 ; i++){
+        console.log(realdata[i].completed)
+    }
+
+}
+
+GETDATA();
